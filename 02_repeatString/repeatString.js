800a56630a40f34f1cff651c2f9205f64a85927d
lubos89 <109877228+lubos89@users.noreply.github.com>
2023-03-17 21:20:48 +0100

    add solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    let result = "";
 
+    if (number < 0 || typeof string !== "string") return "ERROR";
+
+    for (let i = 0; i < number; i++) {
+        result += string;
+    }
+    
+    return result;
 };
 
 // Do not edit below this line
