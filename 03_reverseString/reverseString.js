33e40b870df472e214674615ce4b9e05b6dc119e
lubos89 <109877228+lubos89@users.noreply.github.com>
2023-03-18 11:49:50 +0100

    delete empty line

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,11 +2,10 @@ const reverseString = function(string) {
     const stringArray = string.split("");
     let reversedString = "";
 
-
     for (let i = stringArray.length - 1; i >= 0; i--) {
         reversedString += stringArray[i];
     }
-
+    
     return reversedString;
 };
 
