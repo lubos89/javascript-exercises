a8addef22b6510630bb5bd7ae711ba1d2b2fe518
lubos89 <109877228+lubos89@users.noreply.github.com>
2023-03-22 20:06:31 +0100

    add working solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let result = 0;
 
+    if (!Number.isFinite(start) || !Number.isFinite(end)) return 'ERROR';
+    if (start < 0 || end < 0) return 'ERROR';
+    if (start > end) [start, end] = [end, start];
+
+    for (let i = start; i <= end; i++) {
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
